93fdb0ad9568c99b40c617a05fa1facf3db00f6c
MastaG <mastag@gmail.com>
2017-12-08 07:57:37 +0100

    Use compcache driver from TDT

--- compcache/compat.h
+++ compcache/compat.h
@@ -1,21 +1,23 @@
-#ifndef _ZRAM_COMPAT_H_
-#define _ZRAM_COMPAT_H_
+#ifndef _CCACHE_COMPAT_H_
+#define _CCACHE_COMPAT_H_
 
 #include <linux/version.h>
 
-/* Uncomment this if you are using swap free notify patch */
-#define CONFIG_SWAP_FREE_NOTIFY
-
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,35))
-#ifndef CONFIG_SWAP_FREE_NOTIFY
-#define CONFIG_SWAP_FREE_NOTIFY
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,23)
+#define BIO_IO_ERROR(bio)	bio_io_error(bio, PAGE_SIZE)
+#define BIO_ENDIO(bio, error)	bio_endio(bio, PAGE_SIZE, error)
+#else
+#define BIO_IO_ERROR(bio)	bio_io_error(bio)
+#define BIO_ENDIO(bio, error)	bio_endio(bio, error)
 #endif
+
+#ifndef pr_info
+#define pr_info(fmt, arg...) \
+	printk(KERN_ERR fmt, ##arg)
 #endif
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,31))
-#define blk_queue_physical_block_size(q, size) \
-	blk_queue_hardsect_size(q, size)
-#define blk_queue_logical_block_size(q, size)
+#ifdef bio_discard
+#define SWAP_DISCARD_SUPPORTED
 #endif
 
 #endif
